7ed15fd39ccd59ebc0f0f39497ea0e46010aed20
jedsoft <jedsoft@93520148-bd12-0410-9693-835737c2d474>
2007-06-16 17:23:02 +0000

    slang pre2.1.1 change 5 5. src/slang.ver: The wildcard for the SLANG2 symbols was picking up    the SLANG2.1.0 symbols.  (mlichvar at redhat, com).
    git-svn-id: http://opensvn.csie.org/jedsoft/slang/trunk@125 93520148-bd12-0410-9693-835737c2d474

--- src/slang.h
+++ src/slang.h
@@ -23,7 +23,7 @@ USA.
 */
 
 #define SLANG_VERSION 20101
-#define SLANG_VERSION_STRING "pre2.1.1-4"
+#define SLANG_VERSION_STRING "pre2.1.1-5"
 /* #ifdef __DATE__ */
 /* # define SLANG_VERSION_STRING SLANG_VERSION_STRING0 " " __DATE__ */
 /* #else */
