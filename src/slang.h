e0862104827e4a44ba02b78cfebbde860efa3251
jedsoft <jedsoft@93520148-bd12-0410-9693-835737c2d474>
2007-09-09 16:41:18 +0000

    Version 2.1.2
    git-svn-id: http://opensvn.csie.org/jedsoft/slang/trunk@185 93520148-bd12-0410-9693-835737c2d474

--- src/slang.h
+++ src/slang.h
@@ -23,7 +23,7 @@ USA.
 */
 
 #define SLANG_VERSION 20102
-#define SLANG_VERSION_STRING "pre2.1.2-26"
+#define SLANG_VERSION_STRING "2.1.2"
 /* #ifdef __DATE__ */
 /* # define SLANG_VERSION_STRING SLANG_VERSION_STRING0 " " __DATE__ */
 /* #else */
