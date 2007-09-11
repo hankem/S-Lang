44a44df04c5ae6b866279cdec27807393620678c
jedsoft <jedsoft@93520148-bd12-0410-9693-835737c2d474>
2007-09-11 02:57:09 +0000

    slang pre2.1.3 change 1 1. autoconf/Makefile.in: If any */Makefile.in is more recent than the    corresponding Makefile, request that the configure script be rerun.
    git-svn-id: http://opensvn.csie.org/jedsoft/slang/trunk@186 93520148-bd12-0410-9693-835737c2d474

--- src/slang.h
+++ src/slang.h
@@ -22,8 +22,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
 USA.  
 */
 
-#define SLANG_VERSION 20102
-#define SLANG_VERSION_STRING "2.1.2"
+#define SLANG_VERSION 20103
+#define SLANG_VERSION_STRING "pre2.1.3-1"
 /* #ifdef __DATE__ */
 /* # define SLANG_VERSION_STRING SLANG_VERSION_STRING0 " " __DATE__ */
 /* #else */
