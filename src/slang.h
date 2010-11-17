97ec4c1741f722a18698e3e154f313a6ecb514a2
John E. Davis <jed@jedsoft.org>
2010-11-17 01:03:37 -0500

    slang pre2.2.3 change 100: New colormap (coolwarm.map)

--- src/slang.h
+++ src/slang.h
@@ -23,7 +23,7 @@ USA.
 */
 
 #define SLANG_VERSION 20203
-#define SLANG_VERSION_STRING "pre2.2.3-99"
+#define SLANG_VERSION_STRING "pre2.2.3-100"
 /* #ifdef __DATE__ */
 /* # define SLANG_VERSION_STRING SLANG_VERSION_STRING0 " " __DATE__ */
 /* #else */
