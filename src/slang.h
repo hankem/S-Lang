41f8a2962e96bd4ecfc79d319c3e73535c97be5d
John E. Davis <jed@jedsoft.org>
2010-12-28 17:44:28 -0500

    slang pre2.2.4 change 2: Make libslang.so a symlink to libslang.so.2 instead of libslang.so.2.2.4

--- src/slang.h
+++ src/slang.h
@@ -23,7 +23,7 @@ USA.
 */
 
 #define SLANG_VERSION 20204
-#define SLANG_VERSION_STRING "pre2.2.4-1"
+#define SLANG_VERSION_STRING "pre2.2.4-2"
 /* #ifdef __DATE__ */
 /* # define SLANG_VERSION_STRING SLANG_VERSION_STRING0 " " __DATE__ */
 /* #else */
