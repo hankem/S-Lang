8c715ea98ed70e22bba652600e38d49e5f12a45b
John E. Davis <jed@jedsoft.org>
2012-01-18 20:37:23 -0500

    pre2.3.0-32: badlinks script: GPLv3 -> GPLv2

--- src/slang.h
+++ src/slang.h
@@ -23,7 +23,7 @@ USA.
 */
 
 #define SLANG_VERSION 20300
-#define SLANG_VERSION_STRING "pre2.3.0-31"
+#define SLANG_VERSION_STRING "pre2.3.0-32"
 /* #ifdef __DATE__ */
 /* # define SLANG_VERSION_STRING SLANG_VERSION_STRING0 " " __DATE__ */
 /* #else */
