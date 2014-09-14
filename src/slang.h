edff27366a9d039a3cb9c5d219fdfd3318c45c01
John E. Davis <jed@jedsoft.org>
2014-09-13 22:03:46 -0400

    pre2.3.0 change 130: Updated NEWS file

--- src/slang.h
+++ src/slang.h
@@ -23,7 +23,7 @@ USA.
 */
 
 #define SLANG_VERSION 20300
-#define SLANG_VERSION_STRING "pre2.3.0-130"
+#define SLANG_VERSION_STRING "pre2.3.0-131"
 /* #ifdef __DATE__ */
 /* # define SLANG_VERSION_STRING SLANG_VERSION_STRING0 " " __DATE__ */
 /* #else */
