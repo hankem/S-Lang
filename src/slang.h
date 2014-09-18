e105c969039a1ec7f83a89863c84260934896260
John E. Davis <jed@jedsoft.org>
2014-09-17 23:02:24 -0400

    Version 2.3.0 released

--- src/slang.h
+++ src/slang.h
@@ -23,7 +23,7 @@ USA.
 */
 
 #define SLANG_VERSION 20300
-#define SLANG_VERSION_STRING "pre2.3.0-134"
+#define SLANG_VERSION_STRING "2.3.0"
 /* #ifdef __DATE__ */
 /* # define SLANG_VERSION_STRING SLANG_VERSION_STRING0 " " __DATE__ */
 /* #else */
