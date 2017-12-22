51dfe68ad6e47933838052c6cfd27626407db1fa
John E. Davis <jed@jedsoft.org>
2017-12-22 17:07:17 -0500

    pre2.3.2-18: If the CSV delimiter or quote character is specified to be 0, then use the defaults (, and ").

--- modules/csv-module.c
+++ modules/csv-module.c
@@ -21,6 +21,13 @@ struct _CSV_Type
    int flags;
 };
 
+static int check_special_chars (CSV_Type *csv)
+{
+   if (csv->delimchar == 0) csv->delimchar = ',';
+   if (csv->quotechar == 0) csv->quotechar = '"';
+   return 0;
+}
+
 static int execute_read_callback (CSV_Type *csv, char **sptr)
 {
    char *s;
@@ -346,6 +353,7 @@ static void new_csv_decoder_intrin (void)
    if ((-1 == SLang_pop_int (&csv->flags))
        ||(-1 == SLang_pop_char (&csv->quotechar))
        || (-1 == SLang_pop_char (&csv->delimchar))
+       || (-1 == check_special_chars (csv))
        || (-1 == SLang_pop_anytype (&csv->callback_data))
        || (NULL == (csv->read_callback = SLang_pop_function ()))
        || (NULL == (mmt = SLang_create_mmt (CSV_Type_Id, (VOID_STAR)csv))))
@@ -571,6 +579,7 @@ static void new_csv_encoder_intrin (void)
    if ((-1 == SLang_pop_int (&csv->flags))
        ||(-1 == SLang_pop_char (&csv->quotechar))
        || (-1 == SLang_pop_char (&csv->delimchar))
+       || (-1 == check_special_chars (csv))
        || (NULL == (mmt = SLang_create_mmt (CSV_Type_Id, (VOID_STAR)csv))))
      {
 	free_csv_type (csv);
